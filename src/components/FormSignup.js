b2abd86938074ab2a0bb0501c0321af1981dd00d
DygelnyyKirill <dygelniy.1994kirill@gmail.com>
2021-07-13 22:00:17 +0300

    Editing FormSignup component

--- src/components/FormSignup.js
+++ src/components/FormSignup.js
@@ -3,6 +3,24 @@ const FormSignup = ({ title }) => {
         <div>
             <form className='form'>
                 <h3>{ title }</h3>
+                <div className='form-control'>
+                <label>Login</label>
+                    <input 
+                        type='text'
+                        placeholder='Enter your login'
+                    />
+                </div>
+                <div className='form-control'>
+                <label>Password</label>
+                    <input 
+                        type='text'
+                        placeholder='Enter your password'
+                    />
+                </div>
+                <input type='submit' value='Save Tasks' className='btn' />
+                <span className="form-input">
+                    Already have an account? Login <a href='#'>here</a>
+                </span>
             </form>
         </div>
     )
