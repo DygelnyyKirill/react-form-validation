d4558a8943dd3344fd9a89437ed56ef51d06f08f
DygelnyyKirill <dygelniy.1994kirill@gmail.com>
2021-07-15 21:30:06 +0300

    Create custom useForm hook

--- src/components/FormSignup.js
+++ src/components/FormSignup.js
@@ -1,24 +1,34 @@
+import useForm from './useForm'
+
 const FormSignup = ({ title }) => {
+    const { handleChange, values, handleSubmit } = useForm()
+
     return (
         <div>
-            <form className='form'>
+            <form className='form' onSubmit={handleSubmit}>
                 <h3>{ title }</h3>
                 <div className='form-control'>
                 <label>Login</label>
                     <input 
                         type='text'
                         placeholder='Enter your login'
+                        name='login'
+                        value={values.login}
+                        onChange={handleChange}
                     />
                 </div>
                 <div className='form-control'>
                 <label>Password</label>
                     <input 
-                        type='text'
+                        type='password'
                         placeholder='Enter your password'
+                        name='password'
+                        value={values.password}
+                        onChange={handleChange}
                     />
                 </div>
                 <input type='submit' value='Save Tasks' className='btn' />
-                <span className="form-input">
+                <span className="form-input ">
                     Already have an account? Login <a href='#'>here</a>
                 </span>
             </form>
